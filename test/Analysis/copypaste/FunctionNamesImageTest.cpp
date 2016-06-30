cc923b8dfffbbfbcbbfcf70b6eb2a5837425c0df
Raphael Isemann <teemperor@gmail.com>
2016-06-30 13:13:02 +0200

    Function names test is now using an assert macro.
    Previously, assert was wrongly an function in this test.
    As macros are better support since the previous commit,
    we can also use a more realistic macro here.

--- test/Analysis/copypaste/FunctionNamesImageTest.cpp
+++ test/Analysis/copypaste/FunctionNamesImageTest.cpp
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -analyze -std=c++11 -analyzer-checker=clone.CopyPasteChecker -verify %s
 
-#define GTEST_FOO(TESTNAME, CODE) void test##TESTNAME () { CODE }
+#define GTEST_FOO(TESTNAME, CODE) int test##TESTNAME () { CODE ; return 0; }
 
 
 struct Image {
@@ -10,7 +10,7 @@ struct Image {
   void setWidth(int x) { w = x; }
   void setHeight(int y) { h = y; }
 };
-void assert(bool) {}
+#define assert(ARG) {if (!(ARG)) return 1;}
 
 GTEST_FOO(WidthRanges, { // expected-note{{Suggestion is based on this similar algorithm.}}
   Image img;
